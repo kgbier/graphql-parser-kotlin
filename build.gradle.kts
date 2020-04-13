a73a4aefda470cc939b36deb1bf0e68025f620d4
Konrad Biernacki <kgbier@gmail.com>
2020-04-13 13:02:59 +1000

    Add dependency on `kotlin-reflect`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
-    // Apply the Kotlin JVM plugin to add support for Kotlin.
+    // Apply the Kotlin JVM plugin to add support for Kotlin
     id("org.jetbrains.kotlin.jvm") version "1.3.70"
 
-    // Apply the java-library plugin for API and implementation separation.
+    // Apply the java-library plugin for API and implementation separation
     `java-library`
 }
 
 repositories {
-    // Use jcenter for resolving dependencies.
-    // You can declare any Maven/Ivy/file repository here.
+    // Use jcenter for resolving dependencies
+    // You can declare any Maven/Ivy/file repository here
     jcenter()
 }
 
@@ -16,12 +16,15 @@ dependencies {
     // Align versions of all Kotlin components
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
 
-    // Use the Kotlin JDK 8 standard library.
+    // Kotlin Reflection library
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // Use the Kotlin JDK 8 standard library
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    // Use the Kotlin test library.
+    // Use the Kotlin test library
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 
-    // Use the Kotlin JUnit integration.
+    // Use the Kotlin JUnit integration
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
